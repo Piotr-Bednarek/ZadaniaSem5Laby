b07e420b6055ce1703a776701f937eb4f47278e0
janekandrz <janekandrz@gmail.com>
2025-10-21 11:10:22 +0200

    laby 3 kontyunacja

--- wdpo/lab3/main.py
+++ wdpo/lab3/main.py
@@ -1,4 +1,5 @@
 import cv2 
+import numpy as np
 
 def empty_callback(value):
     pass
@@ -56,9 +57,32 @@ def zad2():
     # closes all windows (usually optional as the script ends anyway)
     cv2.destroyAllWindows()
 
+def zad3():
+    # create a black image, a window
+    img = cv2.imread("lab3/512-512-max.png",cv2.IMREAD_GRAYSCALE)
+    img = img.astype(np.float32)
+    cv2.namedWindow('image')
+
+    while True:
+        # sleep for 10 ms waiting for user to pr ess some key, return -1 on timeout
+        key_code = cv2.waitKey(10)
+        if key_code == 27:
+            # escape key pressed
+            break
+
+        #silly petla 
+        for i in range(1,img.shape[0]-1):
+            for j in range(1,img.shape[1]-1):
+                img[i,j]=(img[i,j]+img[i-1,j]+img[i+1,j]+img[i,j+1]+img[i,j-1]+img[i+1,j+1]+img[i+1,j-1]+img[i-1,j+1]+img[i-1,j-1])/9
+
+        cv2.imshow('image', img)
+        
+    # closes all windows (usually optional as the script ends anyway)
+    cv2.destroyAllWindows()
+
 
 def main():
-    zad2()
+    zad3()
 
 if __name__=="__main__":
     main()
