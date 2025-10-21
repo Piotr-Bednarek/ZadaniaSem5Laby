6b6bb8dd517a78f078d896699533bb8824369898
janekandrz <janekandrz@gmail.com>
2025-10-21 10:54:47 +0200

    laby3 wdpo

--- wdpo/lab2/main.py
+++ wdpo/lab2/main.py
@@ -6,8 +6,7 @@ def empty_callback(value):
     pass
 
 # create a black image, a window
-img = cv2.imread("lab2/AdditiveColor.png",cv2.IMREAD_GRAYSCALE)
-img_scale=cv2.resize(img,dsize=(300,300))
+img = cv2.imread("lab3/512-512-max.png",cv2.IMREAD_GRAYSCALE)
 cv2.namedWindow('image')
 
 # create trackbars for color change
@@ -19,14 +18,18 @@ while True:
     if key_code == 27:
         # escape key pressed
         break
-    cv2.imshow('image', img_scale) 
+    cv2.imshow('image', img) 
 
 
     # get current positions of four trackbars
     val = cv2.getTrackbarPos('progowanie', 'image')
-    _, img_scale =cv2.threshold(img_scale,val,255,cv2.THRESH_BINARY)
+    _, img_scale =cv2.threshold(img,val,255,cv2.THRESH_BINARY)
+
+    kernel=np.ones((5,5),np.uint8)
 
     cv2.imshow("res",img_scale)
+    cv2.imshow('res_erode',cv2.erode(img_scale,kernel))
+    cv2.imshow("res_dilatation",cv2.dilate(img_scale,kernel))
      
 
 # closes all windows (usually optional as the script ends anyway)
---
+++ wdpo/lab3/main.py
@@ -0,0 +1,64 @@
+import cv2 
+
+def empty_callback(value):
+    pass
+
+def zad1():
+
+    # create a black image, a window
+    img = cv2.imread("lab3/512-512-max.png",cv2.IMREAD_GRAYSCALE)
+    cv2.namedWindow('image')
+    cv2.namedWindow('img res')
+
+    # create trackbars for color change
+    cv2.createTrackbar('progowanie', 'image', 0, 5, empty_callback)
+
+    while True:
+        # sleep for 10 ms waiting for user to press some key, return -1 on timeout
+        key_code = cv2.waitKey(10)
+        if key_code == 27:
+            # escape key pressed
+            break
+        cv2.imshow('image', img)
+
+        val = cv2.getTrackbarPos('progowanie', 'image')
+        filsize=2*val+1 
+
+        img_fil=cv2.GaussianBlur(img,(filsize,filsize),2,None,borderType=cv2.BORDER_DEFAULT)
+        cv2.imshow("img res",img_fil)
+        # get current positions of four trackbars
+        
+    # closes all windows (usually optional as the script ends anyway)
+    cv2.destroyAllWindows()
+
+def zad2():
+    # create a black image, a window
+    img = cv2.imread("lab3/512-512-max.png",cv2.IMREAD_GRAYSCALE)
+    cv2.namedWindow('image')
+
+    while True:
+        # sleep for 10 ms waiting for user to pr ess some key, return -1 on timeout
+        key_code = cv2.waitKey(10)
+        if key_code == 27:
+            # escape key pressed
+            break
+
+        #silly petla 
+        for i in range(img.shape[0]):
+            for j in range(img.shape[1]):
+                if i%3==0:
+                    img[i,j]=0
+                if j%3==0:
+                    img[i,j]=255
+
+        cv2.imshow('image', img)
+        
+    # closes all windows (usually optional as the script ends anyway)
+    cv2.destroyAllWindows()
+
+
+def main():
+    zad2()
+
+if __name__=="__main__":
+    main()
